2310a7f816bd17565292e97dfdf3a6f7355d6950
Ben Garnaat <garnaat.ben@gmail.com>
2016-02-22 15:40:10 -0800

    initial eventbrite api commit

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -1,1 +1,34 @@
 // EVENTS
+/*
+  parameter shorthand corresponds to GET /events/search/ paramters.  Reference to https://www.eventbrite.com/developer/v3/endpoints/events/:
+    q: query string
+    vc: venue.city
+    c: categories
+    d: start_date.keyword
+
+
+*/
+
+(function(module) {
+  var events = {};
+
+  events.loadEvents = function(query, page, next) {
+
+    $.ajax({
+      url: 'https://www.eventbriteapi.com/v3/events/search/' +
+            query ? '?' + query : '',
+      type: 'GET',
+      headers: {'Authorization': 'Bearer ' + key_eb},
+      success: function(data, message, xhr) {
+        console.log('xhr: ');
+        console.log(xhr);
+        console.log('data: ');
+        console.log(data);
+      }
+    });
+
+    next();
+  }
+
+  module.events = events;
+})(window);
---
+++ public/scripts/eventsView.js
@@ -0,0 +1,5 @@
+(function(module) {
+  var eventsView = {};
+
+  module.eventsView = eventsView;
+})(window);
