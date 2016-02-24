517f6d5d09b52d236027feb9527585b16f9fbda8
Kent Ross <root.main@gmail.com>
2016-02-24 15:41:56 -0800

    made about loading callback to adding them to the page

--- public/scripts/aboutController.js
+++ public/scripts/aboutController.js
@@ -1,8 +1,11 @@
 (function(module) {
   var aboutController = {};
 
-  localCache.fetch('about', About.loadAll);
-  aboutView.initIndexPage('#about');
+  localCache.fetch('about', function(data) {
+    About.loadAll(data);
+    aboutView.initIndexPage('#about');
+  );
+  // aboutView.initIndexPage('#about');
 
   aboutController.index = function() {
     console.log('jey');
