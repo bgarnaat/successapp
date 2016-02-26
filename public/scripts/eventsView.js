b57d37ce9d94a3b03dbc2537d5303046468a043b
Ben Garnaat <garnaat.ben@gmail.com>
2016-02-25 16:06:45 -0800

    added 'no events' if no events found

--- public/scripts/eventsView.js
+++ public/scripts/eventsView.js
@@ -8,14 +8,20 @@
     $('.event-loader').hide();
     var $eventSection = $('#event-section');
     $eventSection.empty();
-    events.forEach(function(a) {
-      var $eventElement = $(eventsTemplate(a));
-      // attach handler for expanded information
-      $eventElement.click(function() {
-        $(this).find('.event-expand').toggle();
+    if (events === {}) {
+      events.forEach(function(a) {
+        var $eventElement = $(eventsTemplate(a));
+        // attach handler for expanded information
+        $eventElement.click(function() {
+          $(this).find('.event-expand').toggle();
+        });
+        $eventSection.append($eventElement);
       });
-      $eventSection.append($eventElement);
-    });
+    } else {
+      console.log('there were no events');
+      var $noEventElement = $('<p class="no-events">No Events Found</p>');
+      $eventSection.append($noEventElement);
+    }
   };
 
   module.eventsView = eventsView;
